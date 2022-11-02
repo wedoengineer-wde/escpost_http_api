23fbf12d404f49e6d5c0a93a1a0ffec6fc40cf27
Camilo Gomez <caegomezji@gmail.com>
2022-11-01 20:49:00 -0500

    adding docstring to app create_app

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,15 @@ from flask import Flask
 from config import Config
 
 
-def create_app(config: Config):
+def create_app(config: Config) -> Flask:
+    """ A function used to create all the general flask configurations
+
+    Args:
+        config (Config): It recieves the Config file
+
+    Returns:
+        Flask: The app configured
+    """
     app = Flask(__name__)
 
     app.config.from_mapping(config.flask_settings)
