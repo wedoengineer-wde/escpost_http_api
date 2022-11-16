91dfa19cdb55857a454eb290aa80f81e68c4eeae
Camilo Gomez <caegomezji@gmail.com>
2022-11-16 13:29:47 -0500

    not implementing the cut at test

--- test/print_usb_.py
+++ test/print_usb_.py
@@ -4,9 +4,13 @@ id_vendor = 0x0483
 id_product = 0x5743
 
 p = Usb(id_vendor, id_product)
+p.text("ADSFASDFASDFASDFASDFASDF\n")
+p.text("ADSFASDFASDFASDFASDFASDF\n")
+p.text("ADSFASDFASDFASDFASDFASDF\n")
+p.text("ADSFASDFASDFASDFASDFASDF\n")
 # for i in range(10):
 #    p.text("Hello World\n")
 # p.image("logo.gif")
-p.cut()
+# p.cut()
 #p.barcode('1324354657687', 'EAN13', 64, 2, '', '')
 # p.cashdraw(2)
