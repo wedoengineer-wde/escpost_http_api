d27c9268e478993c58bc73bb17891c929ffbaa42
Camilo Gomez <caegomezji@gmail.com>
2022-11-16 13:08:01 -0500

    transforming the params from txt to hex

--- printer_controller.py
+++ printer_controller.py
@@ -2,12 +2,22 @@ from escpos.printer import Dummy, Usb, Serial
 from escpos.escpos import Escpos
 
 
+def string_to_hex(string_var: str):
+    an_integer = int(string_var, 16)
+    #hex_value = hex(an_integer)
+    # print(hex_value)
+    return an_integer
+
+
 def create_printer(printer_driver: str, printer_settings: dict) -> Escpos:
 
     if printer_driver == "Usb":
-        return Usb(*printer_settings)
-
+        printer_settings["idVendor"] = string_to_hex(
+            printer_settings["idVendor"])
+        printer_settings["idProduct"] = string_to_hex(
+            printer_settings["idProduct"])
+        return Usb(**printer_settings)
     if printer_driver == "Serial":
-        return Serial(*printer_settings)
+        return Serial(**printer_settings)
 
     return Dummy()
